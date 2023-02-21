61295ec6b50030848dc53ee68c7e8373170335ab
“Tran <tranvanlong6677@gmail.com>
2023-02-21 12:22:47 +0700

    connection pools

--- src/config/old.js
+++ src/config/old.js
@@ -2,15 +2,12 @@ const mysql = require("mysql2");
 const dotenv = require("dotenv").config();
 
 // create the connection to database
-const connection = mysql.createPool({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
 });
 
 module.exports = connection;
