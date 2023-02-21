6646e7034a4d0ac8b2abdb12679bc460a0a4d373
“Tran <tranvanlong6677@gmail.com>
2023-02-21 12:05:09 +0700

    connection cost

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,11 @@
+const connecttion = required("../config/database.js");
+
 const getHomePage = (req, res) => {
-  res.send("Hello World! long tran111");
+  // simple query
+  connection.query("SELECT * FROM Users ", function (err, results, fields) {
+    console.log("check results", results); // results contains rows returned by server
+    console.log("check fields", fields); // fields contains extra meta data about results, if available
+  });
 };
 
 const getEjs = (req, res) => {
