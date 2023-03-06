e7eee5a77e7e25a54746b3d2681e145fee840a52
“Tran <tranvanlong6677@gmail.com>
2023-03-06 20:29:32 +0700

    navbar

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,11 +1,8 @@
-const connecttion = required("../config/database.js");
+const connection = require("../config/database.js");
 
 const getHomePage = (req, res) => {
   // simple query
-  connection.query("SELECT * FROM Users ", function (err, results, fields) {
-    console.log("check results", results); // results contains rows returned by server
-    console.log("check fields", fields); // fields contains extra meta data about results, if available
-  });
+  return res.render("home.ejs");
 };
 
 const getEjs = (req, res) => {
