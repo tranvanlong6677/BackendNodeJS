61ce98b733b6d716fdb7d6e74175338419ea5d8c
“Tran <tranvanlong6677@gmail.com>
2023-03-07 10:36:08 +0700

    create user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -10,7 +10,21 @@ const getEjs = (req, res) => {
 };
 
 const createUser = (req, res) => {
-  console.log(">>> check req.body", req.body);
+  let email = req.body.email;
+  let name = req.body.name;
+  let city = req.body.city;
+  console.log(">>> check req.body", email, name, city);
+  connection.query(
+    `INSERT INTO Users (email, name, city) VALUES (?,?,?)`,
+    [email, name, city],
+    function (err, results) {
+      console.log(results);
+      console.log(">>> Create user success");
+    }
+  );
+  //   INSERT INTO Users (email, name, city)
+  // VALUES ("long@gmail.com","long","hanam");
+
   res.send("Create new user");
 };
 
